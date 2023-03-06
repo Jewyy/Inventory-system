fdce2ee407df48d84e31c6cce9667e0367d9d470
mmueylii <srimuey@hotmail.com>
2023-03-06 10:13:57 +0700

    crecate page delete

--- delete_product.php
+++ delete_product.php
@@ -1,25 +1,56 @@
 <?php
-  require_once('includes/load.php');
-  $product = find_by_id('products',(int)$_GET['id']);
-  if(!$product){
-    $session->msg("d","Missing Product id.");
+require_once 'includes/load.php';
+$product = find_by_id('products', (int) $_GET['id']);
+if (!$product) {
+    $session->msg('d', 'Missing Product id.');
     redirect('product.php');
-  }
-  
-  // Check if user confirmed the deletion
-  if(isset($_POST['confirm']) && $_POST['confirm'] == 'yes') {
-    $delete_id = delete_by_id('products',(int)$product['id']);
-    if($delete_id){
-      $session->msg("s","Product deleted.");
-      redirect('product.php');
+}
+
+// Check if user confirmed the deletion
+if (isset($_POST['confirm']) && $_POST['confirm'] == 'yes') {
+    $delete_id = delete_by_id('products', (int) $product['id']);
+    if ($delete_id) {
+        $session->msg('s', 'Product deleted.');
+        redirect('product.php');
     } else {
-      $session->msg("d","Product deletion failed.");
-      redirect('product.php');
+        $session->msg('d', 'Product deletion failed.');
+        redirect('product.php');
     }
-  }
+}
 ?>
-<?php include_once('layouts/header.php'); ?>
-<div class="row">
+<?php include_once 'layouts/header.php'; ?>
+<div class="content">
+    <div class="box-header">
+        <div class="headertext">Delete product</div>
+    </div>
+    <div class="max-width">
+        <img style="width: 200px; height: 200px;  display: flex; align-items: center; background-color :aliceblue;"src="libs/images/food.png"
+            alt="">
+        <!-- <imgsrc="uploads/products/<?php echo $product['image']; ?>" alt=""> -->
+        <div class="content-text">Are you sure you want to delete “<?php echo remove_junk(ucfirst($product['name'])); ?>?”</div>
+        <div>
+            <td class="text-center"> In-stock:<?php echo remove_junk($product['quantity']); ?></td>
+            <td class="text-center">| Buying price:<?php echo remove_junk($product['buy_price']); ?></td>
+            <td class="text-center">| Selling price:<?php echo remove_junk($product['sale_price']); ?></td>
+        </div>
+        <div class="group-btn">
+            <form method="post"> <input type="hidden" name="confirm" value="yes">
+                <a href="product.php" class="btn btn-default">Cancel</a>
+                <button type="submit" class="btn btn-danger">Delete product</button>
+            </form>
+        </div>
+        <!-- <div class="panel-body">
+            <form method="post">
+                <div class="form-group">
+                    <input type="hidden" name="confirm" value="yes">
+                    <a href="product.php" class="btn btn-default">Cancel</a>
+                    <button type="submit" class="btn btn-danger">Delete product</button>
+                </div>
+            </form>
+        </div> -->
+    </div>
+</div>
+<!-- <div class="row">
   <div class="col-md-12">
     <?php echo display_msg($msg); ?>
   </div>
@@ -29,7 +60,7 @@
       <div class="panel-heading">
         <strong>
           <span class="glyphicon glyphicon-trash"></span>
-          <span>Are you sure you want to delete <?php echo remove_junk(ucfirst($product['name'])); ?>?</span>
+          <span class="text-content">Are you sure you wajhfnt to delete “<?php echo remove_junk(ucfirst($product['name'])); ?>?”</span>
         </strong>
       </div>
       <div class="panel-body">
@@ -43,5 +74,5 @@
       </div>
     </div>
   </div>
-</div>
-<?php include_once('layouts/footer.php'); ?>
+</div> -->
+<?php include_once 'layouts/footer.php'; ?>
---
+++ layouts/admin_menu.php
@@ -0,0 +1,19 @@
+<?php
+  $toggle=false;
+?>
+<ul>
+  <li>
+    <a href="#" class="submenu-toggle">
+      <span style="padding-left:15px;">Products</span>
+      <svg class="sidebar-arrow" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M3.19997 6.40005C3.19997 6.19525 3.27837 5.99045 3.43437 5.83445C3.74717 5.52165 4.25277 5.52165 4.56557 5.83445L7.99997 9.26885L11.4344 5.83445C11.7472 5.52165 12.2528 5.52165 12.5656 5.83445C12.8784 6.14725 12.8784 6.65285 12.5656 6.96565L8.56557 10.9657C8.25277 11.2785 7.74717 11.2785 7.43437 10.9657L3.43437 6.96565C3.27837 6.80965 3.19997 6.60485 3.19997 6.40005Z" fill="#5C5F62"/>
+      </svg>
+        <!-- <i class="glyphicon glyphicon-chevron-down"></i> -->
+        <!-- <i class="glyphicon glyphicon-chevron-up"></i> -->
+    </a>  
+    <ul class="nav submenu">
+      <a href="product.php"><i class="glyphicon glyphicon-th-large"></i><span href="product.php">Manage Products</span> </a>
+      <a href="add_product.php"><i class="glyphicon glyphicon-th-large"></i><span href="add_product.php">Add Products</span> </a>
+   </ul>
+  </li>
+</ul>
