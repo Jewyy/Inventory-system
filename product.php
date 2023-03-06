8941bff56c31b16594e7c463daeea6dea4406351
Benz <C-benzz>
2023-03-06 11:05:50 +0700

    merge manaageproduct to dev

--- product.php
+++ product.php
@@ -20,6 +20,16 @@
            <input type="text" id="search" placeholder="Search...">
          </div>
         </div>
+  <div class="sidebar">
+    <?php include_once "menu.php"; ?>
+  </div>
+  <div style="background-color: white; display: 100%;">
+    <div class="clearfix" style="padding-bottom: 20px;">
+      <div class="pull-right" style="padding-right: 20px; padding-top: 20px;">
+        <a href="add_product.php" class="btn btn-success"> <img src="./asset/plus.svg">
+          <span style="padding-bottom: 15px;">
+            Add Product
+          </span>
         <div class="panel-body">
           <!-- <table class="table table-bordered"> -->
           <table class="table">
@@ -72,6 +82,56 @@
         </div>
       </div>
     </div>
+    <table class=product-table">
+      <thead>
+        <tr style="height:60px; border-bottom:solid;  border-color:#D3D3D3; box-shadow:0px 3px 0px #E6E6E6">
+          <th class="product-tab" style="width: 30px;">No.</th>
+          <th class="product-tab" style="width: 50px;"> Photo</th>
+          <th class="product-tab" style="width: 20px;"> Product Name </th>
+          <th class="product-tab" style="width: 10%;"> In-stock </th>
+          <th class="product-tab" style="width: 10%;"> Buying Price </th>
+          <th class="product-tab" style="width: 10%;"> Selling Price </th>
+          <th class="product-tab" style="width: 10%;"> Product Add </th>
+          <th class="product-tab" style="width: 10%; text-align: center;"> Action </th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php foreach ($products as $product) : ?>
+          <tr class="product-row-style">
+            <td><?php echo count_id(); ?></td>
+            <td>
+              <?php if ($product["media_id"] === "0") : ?>
+                <img class="img-avatar img-circle" src="uploads/products/no_image.png" alt="">
+              <?php else : ?>
+                <img class="img-avatar img-circle" src="uploads/products/<?php echo $product["image"]; ?>" alt="">
+              <?php endif; ?>
+            </td>
+            <div class="display-box">
+              <td> <?php echo remove_junk($product["name"]); ?>
+              <br>
+                <span class="category-button">
+                  <?php echo remove_junk($product["categorie"]); ?>
+                </span>
+              </td>
+            </div>
+            <td class="display-font-table"> <?php echo remove_junk($product["quantity"]); ?></td>
+            <td class="display-font-table"> <?php echo remove_junk($product["buy_price"]); ?></td>
+            <td class="display-font-table"> <?php echo remove_junk($product["sale_price"]); ?></td>
+            <td style="font-weight:bold;"> <?php echo read_date($product["date"]); ?></td>
+            <td>
+              <div class="btn-group" style="padding-left: 15px;">
+                <a href="edit_product.php?id=<?php echo (int) $product["id"]; ?>" class="btn btn-light" title="Edit" data-toggle="tooltip">
+                  <img src="./asset/edit.svg">
+                </a>
+                <a href="delete_product.php?id=<?php echo (int) $product["id"]; ?>" class="btn btn-light" title="Delete" data-toggle="tooltip">
+                  <img src="./asset/trash.svg">
+                </a>
+              </div>
+            </td>
+          </tr>
+        <?php endforeach; ?>
+      </tbody>
+      </tabel>
   </div>
   <?php include_once('layouts/footer.php'); ?>
   
