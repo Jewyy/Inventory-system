e57bb8e55598bfaf5d15455d7dc3afccd1ee9b47
mmueylii <srimuey@hotmail.com>
2023-03-12 22:17:01 +0700

    add picture in delete product

--- delete_product.php
+++ delete_product.php
@@ -1,9 +1,11 @@
 <?php
 require_once 'includes/load.php';
+$products = join_product_table();
 $product = find_by_id('products', (int) $_GET['id']);
 if (!$product) {
     $session->msg('d', 'Missing Product id.');
     redirect('product.php');
+    
 }
 
 // Check if user confirmed the deletion
@@ -24,9 +26,24 @@ if (isset($_POST['confirm']) && $_POST['confirm'] == 'yes') {
         <div class="headertext">Delete product</div>
     </div>
     <div class="max-width">
-        <img style="width: 200px; height: 200px;  display: flex; align-items: center; background-color :aliceblue;"src="libs/images/food.png"
-            alt="">
-        <!-- <imgsrc="uploads/products/<?php echo $product['image']; ?>" alt=""> -->
+    <!-- <tbody>
+          <?php foreach ($products as $product) : ?>
+            <tr class="product-row-style">
+              <td><?php echo count_id(); ?></td>
+              <td>
+                <?php if ($product["media_id"] === "0") : ?>
+                  <img class="img-avatar img-circle" src="uploads/products/no_image.png" alt="">
+                <?php else : ?>
+                  <img class="img-avatar img-circle" src="uploads/products/<?php echo $product["image"]; ?>" alt="">
+                <?php endif; ?>
+              </td>
+          <?php endforeach; ?>
+        </tbody> -->
+        <!-- <img style="width: 200px; height: 200px;  display: flex; align-items: center; background-color :aliceblue;"src="libs/images/food.png"
+            alt=""> -->
+            
+        <img src="uploads/products/<?php echo $product['image']; ?>" style ="width:200px; height:200px;"alt="">
+        <!-- <img class="img-avatar img-circle" src="uploads/products/<?php echo $product["image"]; ?>" alt=""> -->
         <div class="content-text">Are you sure you want to delete “<?php echo remove_junk(ucfirst($product['name'])); ?>?”</div>
         <div>
             <td class="text-center"> In-stock:<?php echo remove_junk($product['quantity']); ?></td>
