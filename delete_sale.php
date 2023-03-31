a1a7582af5ea2676016717d06aa381011e7ebf0e
Tawan <62010308@kmitl.ac.th>
2023-03-31 11:22:35 +0700

    create delete_sale

---
+++ delete_sale.php
@@ -0,0 +1,57 @@
+<?php
+require_once('includes/load.php');
+
+// Checkin What level user has permission to view this page
+// page_require_level(3);
+
+$sale = find_all_sale();
+$sale = find_by_id('sales', (int) $_GET['id']);
+
+if (!$sale) {
+  $session->msg("d", "Missing sale id.");
+  redirect('sales.php');
+}
+
+// Check if user confirmed the deletion
+if (isset($_POST['confirm']) && $_POST['confirm'] == 'yes') {
+  $delete_id = delete_by_id('sales', (int)$sale['id']);
+  if ($delete_id) {
+    $session->msg("s", "Sale ".$sale['name']." deleted.");
+    redirect('sales.php');
+  } else {
+    $session->msg("d", "Sale deletion failed.");
+    redirect('sales.php');
+  }
+}
+?>
+
+
+<?php include_once('layouts/header.php'); ?>
+<div class="row">
+  <div class="col-md-12">
+    <?php echo display_msg($msg); ?>
+  </div>
+  <div class="col-md-6">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <strong>
+          <span class="glyphicon glyphicon-trash"></span>
+          <span>Are you sure you want to delete <?php echo remove_junk($sale['name']); ?>?</span>
+          <!-- <?php foreach ($sale as $sale): ?>
+             
+          <?php endforeach; ?> -->
+        </strong>
+      </div>
+      <div class="panel-body">
+        <form method="post">
+          <div class="form-group">
+            <input type="hidden" name="confirm" value="yes">
+            <button type="submit" class="btn btn-danger">Yes</button>
+            <a href="sales.php" class="btn btn-default">No</a>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+<?php include_once('layouts/footer.php'); ?>
