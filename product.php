60ebe05d9c27639003779764370c48a98907aaf3
Tawan <106691482+tawanwjt@users.noreply.github.com>
2023-04-27 14:40:02 +0700

    Change format

--- product.php
+++ product.php
@@ -11,7 +11,7 @@ $products = join_product_table();
   </div>
   <div class="clearfix">
     <div class="pull-left">
-      <div  class="headertext">
+      <div class="headertext">
         Manage Products
       </div>
     </div>
